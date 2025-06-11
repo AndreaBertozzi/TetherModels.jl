6c3e05415fb6e14a16401ec207d67276f57d5626
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-06-11 11:00:59 +0200

    Add TODO

--- mwes/mwe_01.jl
+++ mwes/mwe_01.jl
@@ -4,3 +4,12 @@ using KiteUtils
 set_data_path("data")
 set = load_settings("system.yaml")
 println(set.d_tether)
+
+ts = TetherModels.TetherSettings()
+ts.cd_tether = set.cd_tether
+ts.d_tether = set.d_tether
+ts.rho_tether = set.rho_tether
+
+
+# TODO calculate rho as function of height
+# TODO calculate c_spring as function of d_tether, rho_tether and segment length
