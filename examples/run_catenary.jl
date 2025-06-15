f14d233a4d55d678e79eeacce7151f82f6c64761
AndreaBertozzi <a.bertozzi@student.tudelft.nl>
2025-06-15 11:20:48 +0200

    Temporarily adjsuted the example to the new interface

--- examples/run_catenary.jl
+++ examples/run_catenary.jl
@@ -7,19 +7,31 @@ tether_length = norm(kite_pos)*1.05
 set_data_path("data")
 settings = load_settings("system.yaml")
 
-state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = init_quasistatic(kite_pos, tether_length, segments = 22, settings=settings)
-state_vec, tether_pos, Ft_ground, Ft_kite, p0 =  simulate_tether(state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings)
+state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = init_quasistatic(kite_pos, tether_length, segments = 20, settings=settings)
+tether = Tether()
+tether.state_vec = state_vec
+tether.tether_length = tether_length
+tether.settings = settings
+tether.segments = 20
+
+v_wind_gnd = 0.0
+wind_dir = 0.0
+
+step!(tether, kite_pos, kite_vel, v_wind_gnd, wind_dir; prn=false)
+
+state_vec = tether.state_vec
+tether_pos = tether.tether_pos
 
 
 x_qs = vec(sqrt.(tether_pos[1,:].^2 + tether_pos[2,:].^2))
 y_qs = vec(tether_pos[3,:])
 
 
-tether_pos = hcat(p0, tether_pos, [0; 0; 0])
+tether_pos = hcat(tether_pos, [0; 0; 0])
 plt.figure("3D view").add_subplot(projection="3d").set_aspect("equal")
 plt.plot3D(tether_pos[1,:], tether_pos[2,:], tether_pos[3,:], marker = "o")
 plt.scatter3D(0, 0, 0,  s = 200, marker = "s", c = "C7")
-plt.scatter3D(p0[1], p0[2], p0[3], s = 50, marker = "D", c = "g")
+#plt.scatter3D(p0[1], p0[2], p0[3], s = 50, marker = "D", c = "g")
 plt.xlabel("X [m]")
 plt.ylabel("Y [m]")
 plt.xlim(0, 100)
