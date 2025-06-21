892dd2e81c734e11c55c74dc8ea65a97a7fce7b6
AndreaBertozzi <a.bertozzi@student.tudelft.nl>
2025-06-21 16:13:00 +0200

    Refactor run_catenary example to include atmospheric model and update settings

--- examples/run_catenary.jl
+++ examples/run_catenary.jl
@@ -1,43 +1,84 @@
-using ControlPlots, TetherModels, StaticArrays, LinearAlgebra, KiteUtils
+using ControlPlots, TetherModels, StaticArrays, LinearAlgebra, KiteUtils, AtmosphericModels
 
-# Set initial conditions
-kite_pos = MVector{3}([100.0, 100, 800])
-tether_length = norm(kite_pos)*1.05
+# Set example kite_pos and kite_vel
+kite_pos = MVector{3}([100.0, 100.0, 300.0])
+kite_vel = MVector{3}([0.5, 0., 0])
 
+# Set wind speed and direction
+v_wind_gnd = 1.0
+wind_dir = pi/4
+
+# Load settings using KiteUtils
 set_data_path("data")
 settings = load_settings("system.yaml")
+
+# Change some default settings
 settings.l_tether = norm(kite_pos)*1.05
-settings.segments = 100
+settings.segments = 20
 
-state_vec, kite_pos, kite_vel, wind_vel, tether_length, settings = init_quasistatic(kite_pos, tether_length, segments = 10, set=settings)
+# Create atmospheric model using AtmosphericModels
+am = AtmosphericModel()
 
-settings = tether_set_from_set(settings)
-tether = Tether(settings, state_vec = state_vec)
+# Define tether object and initialise with analytic catenary shape
+tether = Tether(settings, am)
+init_tether!(tether, kite_pos; kite_vel)
 
-v_wind_gnd = 0.0
-wind_dir = 0.0
+# Plot initial condition of catenary
+plt.figure("3D view").add_subplot(projection="3d").set_aspect("equal")
+plt.plot3D(tether.tether_pos[1,:], tether.tether_pos[2,:], tether.tether_pos[3,:], marker = "+")
 
+# Run model iteration 
 step!(tether, kite_pos, kite_vel, v_wind_gnd, wind_dir; prn=false)
 
-state_vec = tether.state_vec
-tether_pos = tether.tether_pos
-
-
-x_qs = vec(sqrt.(tether_pos[1,:].^2 + tether_pos[2,:].^2))
-y_qs = vec(tether_pos[3,:])
-
-
-tether_pos = hcat(tether_pos, [0; 0; 0])
-plt.figure("3D view").add_subplot(projection="3d").set_aspect("equal")
-plt.plot3D(tether_pos[1,:], tether_pos[2,:], tether_pos[3,:], marker = "o")
+# Plot model after iteration
+plt.plot3D(tether.tether_pos[1,:], tether.tether_pos[2,:], tether.tether_pos[3,:], marker = "o")
 plt.scatter3D(0, 0, 0,  s = 200, marker = "s", c = "C7")
-#plt.scatter3D(p0[1], p0[2], p0[3], s = 50, marker = "D", c = "g")
 plt.xlabel("X [m]")
 plt.ylabel("Y [m]")
 plt.xlim(0, 100)
 plt.ylim(0, 100)
-plt.zlim(0, 800)
-
-plt.legend(["Tether", "Origin", "Kite"])
+plt.zlim(0, 350)
 plt.show()
 
+
+"""
+    get_initial_conditions(filename)
+
+Loads the initialization data for the basic examples and tests
+
+# Arguments
+- filename: the filename of the mat file to read
+
+# Returns
+- state_vec::MVector{3, Float64} state vector (theta [rad], phi [rad], Tn [N])  
+  tether orientation and tension at ground station
+- kite_pos::MVector{3, Float64} kite position vector in wind reference frame
+- kite_vel::MVector{3, Float64} kite velocity vector in wind reference frame
+- wind_matrix::MMatrix{3, segments, Float64} wind velocity vector in wind reference frame for each segment of the tether
+- l_tether: Float64 tether length
+- set::TetherSettings struct containing environmental and tether parameters: see [TetherSettings](@ref)
+"""
+function get_initial_conditions(filename)
+    vars = matread(filename) 
+    state_vec = MVector{3}(vec(get(vars,"stateVec", 0)))
+    kite_pos = MVector{3}(vec(get(vars,"kitePos", 0)))
+    kite_vel = MVector{3}(vec(get(vars,"kiteVel", 0)))
+    wind_matrix = get(vars,"windVel", 0)
+    l_tether = get(vars,"tetherLength", 0)
+
+    ENVMT = get(vars,"ENVMT", 0) 
+    rho_air = get(ENVMT, "rhos", 0) 
+    g_earth = [0; 0; -abs(get(ENVMT, "g", 0))]      # in this way g_earth is a vector [0; 0; -9.81]
+
+    T = get(vars,"T", 0)
+    cd_tether = get(T, "CD_tether", 0) 
+    d_tether = get(T, "d_tether", 0)*1000           # tether diameter                  [mm]
+    rho_tether = get(T, "rho_t", 0) 
+    E = get(T, "E", 0) 
+    A = get(T, "A", 0)
+    c_spring = E*A 
+
+    set = TetherSettings(rho_air, cd_tether, d_tether, rho_tether, c_spring)
+
+    return state_vec, kite_pos, kite_vel, wind_matrix, l_tether, set
+end
